27282280a6126c52d74ad71a8b0ed93834ca3a49
Caio <ccaiocsermak@gmail.com>
2025-04-08 14:07:40 -0300

    modificada classe cliente

--- Cliente.java
+++ Cliente.java
@@ -1,32 +1,30 @@
-public class Cliente implements Cloneable {
-    private int id;
+public class Cliente {
     private String nome;
-    private String email;
+    private String cpf;
 
-    public Cliente(int id, String nome, String email) {
-        this.id = id;
+    public Cliente(String nome, String cpf) {
         this.nome = nome;
-        this.email = email;
+        this.cpf = cpf;
     }
 
-    public int getId() { return id; }
-    public String getNome() { return nome; }
-    public String getEmail() { return email; }
+    public String getNome() {
+        return nome;
+    }
 
-    public void setNome(String nome) { this.nome = nome; }
-    public void setEmail(String email) { this.email = email; }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
     @Override
     public String toString() {
-        return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + "]";
-    }
-
-    @Override
-    public Cliente clone() {
-        try {
-            return (Cliente) super.clone();
-        } catch (CloneNotSupportedException e) {
-            throw new AssertionError();
-        }
+        return "Nome: " + nome + ", CPF: " + cpf;
     }
 }
